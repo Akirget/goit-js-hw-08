9e3a16f760a6d1daf11cb5ef2f7de3c1b16a7944
Kirhet Oleksandr <57485036+Akirget@users.noreply.github.com>
2022-09-26 16:12:24 +0200

    Домашнє завдання JS №8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,7 +3,6 @@ import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
-console.log(galleryItems);
 
 const gallery = document.querySelector('.gallery');
 const imageMarkup = createItemsMarkup(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import '../css/common.css';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function playPlayer(item) {
+  const currentTime = item.seconds;
+  localStorage.setItem('videoplayer-time', currentTime);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-time'));
+
+player.on('timeupdate', throttle(playPlayer, 1000));
