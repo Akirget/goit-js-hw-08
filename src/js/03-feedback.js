8502ea0e1cdf8376617b26db87174348638505af
Kirhet Oleksandr <57485036+Akirget@users.noreply.github.com>
2022-09-27 21:03:35 +0200

    Домашнє завдання JS №8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -18,12 +18,18 @@ function saveFormValue() {
   localStorage.setItem(key, JSON.stringify(formUser));
 }
 
+const parselForm = JSON.parse(localStorage.getItem(key)) || formUser;
+
+if (parselForm) {
+  input.value = parselForm.email;
+  textarea.value = parselForm.message;
+}
+
 form.addEventListener('submit', addSubmitForm);
 
 function addSubmitForm(event) {
   event.preventDefault();
 
-  const parselForm = JSON.parse(localStorage.getItem(key));
   console.log(parselForm);
 
   event.target.reset();
